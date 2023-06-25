267c09ec174bf81b69dd29f4e9fb9c93bdd455be
ReiRev <99321158+ReiRev@users.noreply.github.com>
2023-06-25 20:54:52 +0900

    Chapter2 (#19)
    * make chapter 2
    
    * add index for chapter2.md
    
    * add Casts
    
    * add description for Casts
    
    * add Custom Cast and Value Object
    
    * add new in get methods
    
    * remove duplicate import

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Casts\UserName as UserNameCast;
-use App\Casts\UserId as UserIdCast;
-
-use App\Casts\UserName as UserNameCast;
 
 class User extends Model
 {
