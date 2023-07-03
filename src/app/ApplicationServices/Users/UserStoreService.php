02a9e04e74edb186648e5c943585146af2776b56
ReiRev <99321158+ReiRev@users.noreply.github.com>
2023-07-03 13:05:01 +0900

    Dev chapter6 (#23)
    * formatted
    
    * delete user service
    
    * tmp

---
+++ src/app/ApplicationServices/Users/UserStoreService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\ApplicationServiecs\Users;
+
+use App\Models\User;
+use App\DataTransferObjects\UserData;
+
+class UserStoreService implements UserStoreServiceInterface
+{
+    public function store(Userdata $userdata): void
+    {
+    }
+}
